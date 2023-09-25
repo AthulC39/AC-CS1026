2b12a554799a0d048d976d912ef832ebf14ba0f4
athul charanthara <athulcharanthara@dyn172-30-72-51.wireless.uwo.pri>
2023-09-25 10:33:19 -0400

    first commit

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+shopName = input("Please enter the shop name: ")
+
+ringQTY = (input("Please enter the ring QTY: "))
+glassesQTY = (input("Please enter the glasses QTY: "))
+
+print("Shop name is {}".format(shopName))
+print("Ring QTY is {}".format(ringQTY))
+print("Glasses QTY is {}".format(glassesQTY))
+
+print("Inventory total: {}".format(ringQTY + glassesQTY))
